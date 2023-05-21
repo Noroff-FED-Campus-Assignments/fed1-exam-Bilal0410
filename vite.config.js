8430ed53d40523eecd843d3b6d354c735c29af65
Bilal0410 <bilaln0410@gmail.com>
2023-05-21 03:35:05 +0200

    importing resolve to all pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        // ADD YOUR PAGES HERE
+        list: resolve(_dirname, "list.html"),
+        details: resolve(_dirname, "details.html"),
+        about: resolve(_dirname, "about.html"),
+        contact: resolve(_dirname, "contact.html")
+        
         // contact: resolve(__dirname, "contact.html"),
         // blog: resolve(__dirname, "blogs.html"),
         // blogDetail: resolve(__dirname, "blog-detail.html"),
