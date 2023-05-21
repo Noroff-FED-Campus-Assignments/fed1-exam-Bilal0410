ca35873eff54b86119bfe90a10fa48a95004b692
Bilal0410 <bilaln0410@gmail.com>
2023-05-21 06:43:05 +0200

    search form with valid input field

--- js/app.js
+++ js/app.js
@@ -53,38 +53,28 @@ https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#
  */
 
  const resultsContainer = document.querySelector(".swiper-wrapper");
-
+ const searchInput = document.getElementById("searchInput");
+ const searchButton = document.getElementById("searchButton");
+ 
  const postsUrl = "http://fastcars.local/wp-json/wp/v2/posts/?per_page=12";
  const mediaUrl = "http://fastcars.local/wp-json/wp/v2/media/";
+ 
+ let media = [];
+ let posts = [];
  
  async function fetchBlogsAndInitializeSwiper() {
    try {
      const postsResponse = await fetch(postsUrl);
-     const posts = await postsResponse.json();
+     posts = await postsResponse.json();
  
      const mediaResponse = await fetch(mediaUrl);
-     const media = await mediaResponse.json();
+     media = await mediaResponse.json();
  
-     let html = "";
-     posts.forEach((blog) => {
-       const featuredImageId = blog.featured_media;
-       const featuredImage = media.find((image) => image.id === featuredImageId);
-       const imageUrl = featuredImage ? featuredImage.source_url : "";
-       const title = blog.title.rendered;
+     renderPosts(posts);
  
-       html += `
-         <div class="swiper-slide">
-           <div class="carousel-post">
-             <div class="blogContainer">
-               <a href="/details.html"><img src="${imageUrl}" alt="Featured Image">
-               <h1 class="car-title">${title}</h1></a>
-             </div>
-           </div>
-         </div>
-       `;
-     });
+     searchInput.removeEventListener("input", handleSearchInput);
  
-     resultsContainer.innerHTML = html;
+     searchInput.addEventListener("input", handleSearchInput);
  
      return { media, posts };
    } catch (error) {
@@ -93,36 +83,81 @@ https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#
    }
  }
  
- fetchBlogsAndInitializeSwiper()
-   .then(({ media, posts, error }) => {
+ function filterPostsByTitle(searchText) {
+   return posts.filter((post) =>
+     post.title.rendered.toLowerCase().includes(searchText)
+   );
+ }
+ 
+ function renderPosts(posts) {
+   let html = "";
+   posts.forEach((blog) => {
+     const featuredImageId = blog.featured_media;
+     const featuredImage = media.find((image) => image.id === featuredImageId);
+     const imageUrl = featuredImage ? featuredImage.source_url : "";
+     const title = blog.title.rendered;
+ 
+     html += `
+       <div class="swiper-slide">
+         <div class="carousel-post">
+           <div class="blogContainer">
+             <a href="/details.html"><img src="${imageUrl}" alt="Featured Image"></a>
+             <h1 class="car-title">${title}</h1>
+           </div>
+         </div>
+       </div>
+     `;
+   });
+ 
+   resultsContainer.innerHTML = html;
+ 
+   initializeSwiper();
+ }
+ 
+ async function fetchPostsAndRender() {
+   try {
+     const { media, posts, error } = await fetchBlogsAndInitializeSwiper();
      if (error) {
        console.error("Error:", error);
        return;
      }
+   } catch (error) {
+     console.error("Error:", error);
+   }
+ }
  
-     console.log("Media:", media);
-     console.log("Posts:", posts);
+ function initializeSwiper() {
+   new Swiper(".swiper-container", {
+     slidesPerView: 4,
+     spaceBetween: 10,
+     loop: true,
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true
+     },
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev"
+     },
+     autoplay: {
+       delay: 2500,
+       disableOnInteraction: false
+     },
+     touch: {
+       enabled: true
+     }
+   });
+ }
  
-     new Swiper(".swiper-container", {
-       slidesPerView: 4,
-       spaceBetween: 10,
-       loop: true,
-       pagination: {
-         el: '.swiper-pagination',
-         clickable: true
-       },
-       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-       },
-       autoplay: {
-        delay: 2000,
-        disableOnInteraction: false
-       },
-       touch: {
-        enabled: true
-       }
-     });
+ function handleSearchInput() {
+   const searchText = searchInput.value.trim().toLowerCase();
+   const filteredPosts = filterPostsByTitle(searchText);
+   renderPosts(filteredPosts);
+ }
+ 
+ fetchPostsAndRender()
+   .then(() => {
+     initializeSwiper();
    })
    .catch((error) => {
      console.error("Error:", error);
