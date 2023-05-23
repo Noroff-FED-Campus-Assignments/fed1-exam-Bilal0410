2b27631278329f5b374fae4f01437df888baafd8
Bilal0410 <bilaln0410@gmail.com>
2023-05-23 09:21:54 +0200

    contact form with js validation

---
+++ js/form.js
@@ -0,0 +1,108 @@
+/*
+============================================
+Constants
+@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/advanced-form.html#L50
+============================================
+*/
+
+// TODO: Get DOM elements from the DOM
+
+// TODO: Create event listeners for the form
+
+/*
+============================================
+API calls
+@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/advanced-form.html#L157
+============================================
+*/
+
+// TODO: Set up a function to fetch data from the API
+
+/*
+============================================
+Helper functions
+@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/advanced-form.html#L118
+============================================
+*/
+
+// TODO: Create a function to validate an input field
+
+// TODO: Create a function to create a DOM element
+
+const form = document.querySelector("#contactForm");
+const Name = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (checkLength(Name.value, 1) === true) {
+    nameError.style.display = "none";
+  } else {
+    nameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(subject.value, 14) === true) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (checkLength(message.value, 24) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+
+  if (
+    checkLength(Name.value, 1) &&
+    validateEmail(email.value) &&
+    checkLength(subject.value, 14) &&
+    checkLength(message.value, 24)
+  ) {
+    alert("Form submitted successfully!");
+
+    form.reset();
+  }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+Name.addEventListener("blur", (event) => {
+  const name = event.target.value.trim();
+  const minLengthRegex = /^[a-zA-Z]{2,}$/;
+
+  if (minLengthRegex.test(name)) {
+    event.target.classList.add("is-success");
+    event.target.classList.remove("is-error");
+  } else {
+    event.target.classList.add("is-error");
+    event.target.classList.remove("is-success");
+  }
+});
